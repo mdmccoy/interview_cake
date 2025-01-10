d11f30db0daec2c943a1b5b8e88b00e6e83d9f61
Matt McCoy <mdmccoy@gmail.com>
2025-01-10 18:20:18 -0500

    Reworking word cloud solution

--- word_cloud.rb
+++ word_cloud.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # You want to build a word cloud, an infographic where the size of a word corresponds to how often it appears in the body of text.
 
 # To do this, you'll need data. Write code that takes a long string and builds its word cloud data in a hash ↴ , where the keys are words and the values are the number of times the words occurred.
@@ -14,39 +12,76 @@
 
 # Assume the input will only contain words and standard punctuation.
 
-# Matt's Note
-#
-# The instructions for this excercise are not, in my opinion, in accordance to the tests. The instructions
-# ask the user to make reasonable descisions about capitalization which is subjective, but the tests enforce a specific
-# pattern of handling capitalization: If the word appears once leave it capitalized, if the word appears multiple times it should be downcased.
+# Break the problem down into smaller pieces
+# Split the words
+#    What are the word characters
+# Add the words to a dictionary
+# Decide on what to do about punctuation and capitalization
 
 class WordCloudData
   attr_reader :words_to_counts
 
-  SKIPPABLE_CHARACTERS = %w[-].freeze
-  PUNCTUATION = /[!?.()*&:]/.freeze
+  # SKIPPABLE_CHARACTERS = %w[-].freeze
+  # PUNCTUATION = /[!?.()*&:]/.freeze
+
+  # def initialize(input_string)
+  #   # Count the frequency of each word.
+  #   @words_to_counts = {}
+
+  #   string_array = input_string.split(' ')
+  #   string_array = input_string.split('...') if string_array.size == 1
+
+  #   string_array.each do |word|
+  #     clean_word = word.gsub(PUNCTUATION, '')
+
+  #     next if SKIPPABLE_CHARACTERS.include?(word)
+
+  #     # handle capitalization in the wierd way the tests want you to
+  #     if @words_to_counts[clean_word.capitalize]
+  #       value = @words_to_counts[clean_word.capitalize]
+  #       @words_to_counts.delete(clean_word.capitalize)
+
+  #       @words_to_counts[clean_word] = value
+  #     end
+
+  #     @words_to_counts[clean_word] = @words_to_counts[clean_word] ? @words_to_counts[clean_word] + 1 : 1
+  #   end
+  # end
 
   def initialize(input_string)
-    # Count the frequency of each word.
     @words_to_counts = {}
+    p input_string
+    split_words(input_string)
+  end
 
-    string_array = input_string.split(' ')
-    string_array = input_string.split('...') if string_array.size == 1
-
-    string_array.each do |word|
-      clean_word = word.gsub(PUNCTUATION, '')
-
-      next if SKIPPABLE_CHARACTERS.include?(word)
-
-      # handle capitalization in the wierd way the tests want you to
-      if @words_to_counts[clean_word.capitalize]
-        value = @words_to_counts[clean_word.capitalize]
-        @words_to_counts.delete(clean_word.capitalize)
-
-        @words_to_counts[clean_word] = value
+  def split_words(input_string)
+    word = ''
+    input_string.each_char.with_index do |char, index|
+      if word_character?(char)
+        word << char
+      else
+        add_to_hash(word) unless word.empty?
+        word = ''
       end
 
-      @words_to_counts[clean_word] = @words_to_counts[clean_word] ? @words_to_counts[clean_word] + 1 : 1
+      add_to_hash(word) if index + 1 == input_string.size && word_character?(char)
+    end
+  end
+
+  def word_character?(char)
+    true if char == "'" || !(char =~ /\w/).nil?
+  end
+
+  def add_to_hash(word)
+    # Only make a word uppercase in our hash if it is always uppercase in the original string
+    if @words_to_counts[word.downcase]
+      @words_to_counts[word.downcase] += 1
+    elsif @words_to_counts.include?(word.capitalize)
+      value = @words_to_counts[word.capitalize]
+      @words_to_counts.delete(word.capitalize)
+      @words_to_counts[word] = value + 1
+    else
+      @words_to_counts[word] = @words_to_counts.include?(word) ? @words_to_counts[word] += 1 : 1
     end
   end
 end
@@ -77,7 +112,6 @@ def run_tests
     'cake' => 2
   }
   assert_equal(actual, expected, desc)
-
   desc = 'punctuation'
   input = 'Strawberry short cake? Yum!'
 
@@ -108,6 +142,14 @@ def run_tests
   word_cloud = WordCloudData.new(input)
   actual = word_cloud.words_to_counts
   expected = { 'Bakery' => 1, 'Cakes' => 1, "Allie's" => 1, "Sasha's" => 1 }
+  assert_equal(actual, expected, desc)
+
+  desc = 'downcase then uppercase'
+  input = "I like the cake. The cake is a lie."
+
+  word_cloud = WordCloudData.new(input)
+  actual = word_cloud.words_to_counts
+  expected = { 'I' => 1, 'cake' => 2, "the" => 2, "like" => 1, "is" => 1, "a" => 1, "lie" => 1 }
   assert_equal(actual, expected, desc)
 end
 
